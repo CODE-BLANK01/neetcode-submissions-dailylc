a9b3180b0ab15f403eae06b06c156d3dc7def029
Abdullah Basarvi <blankdevtech@gmail.com>
2026-04-01 17:25:25 -0700

    Bulk sync: 45 submissions

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-3.py
@@ -0,0 +1,26 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid :
+            return 0
+        rows,cols=len(grid), len(grid[0])
+        count =0
+
+
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j]=="1":
+                    count+=1
+
+                    stack=[(i,j)]
+                    grid[i][j]="0"
+                    while stack:
+                        x,y=stack.pop()
+
+                        for dx, dy in ((1,0),(0,1),(-1,0),(0,-1)):
+                            nx,ny=x+dx, y+dy
+
+                            if 0<=nx<rows and 0<=ny<cols and grid[nx][ny]=="1":
+                                grid[nx][ny]="0"
+                                stack.append((nx,ny))
+        return count
+                    
---
+++ Data Structures & Algorithms/last-stone-weight/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        for i in range(len(stones)):
+            stones[i]=-stones[i]
+
+        heapq.heapify(stones)
+
+        while len(stones)>1:
+            x=(heapq.heappop(stones))
+            y=(heapq.heappop(stones))
+            heapq.heappush(stones, x-y)
+        stones.append(0)
+        return abs(stones[0])
